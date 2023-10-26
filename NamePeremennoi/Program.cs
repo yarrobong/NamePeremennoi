11157b2dd52ca8c7e82b9057a8bff543c37dccd8
yarrobong <oneboardsholi@gmail.com>
2023-10-26 13:13:58 +0500

    Добавьте файлы проекта.

---
+++ NamePeremennoi/Program.cs
@@ -0,0 +1,18 @@
+﻿string str = Console.ReadLine();
+string pattern = "qwertyuiopasdfghjklzxcvbnm1234567890_";
+str = str.ToLower();
+if (Char.IsDigit(str.First()))
+{
+    Console.WriteLine("NO");
+    return;
+}
+foreach (char c in str)
+{
+    // Если символ не содержится в pattern, выводим "NO" 
+    if (pattern.IndexOf(c) == -1)
+    {
+        Console.WriteLine("NO");
+        return;
+    }
+}
+Console.WriteLine("YES");
